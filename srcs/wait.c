6e57f373a85bef5f1fdbb12aee3d2147717c2e99
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-04-05 21:54:39 +0200

    fix: exit status if child terminated with signal

--- srcs/wait.c
+++ srcs/wait.c
@@ -23,8 +23,13 @@ int	wait_for_all_childs(int child_count, int lastpid)
 	exit_code = 1;
 	while (i < child_count)
 	{
-		if (waitpid(-1, &status, 0) == lastpid && WIFEXITED(status))
+		if (waitpid(-1, &status, 0) == lastpid)
+		{
+			if (WIFEXITED(status))
 				exit_code = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				exit_code = WTERMSIG(status) + 128;
+		}
 		if (WIFEXITED(status) || WIFSIGNALED(status))
 			i++;
 	}
